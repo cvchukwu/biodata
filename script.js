999bce3f88d89ccf5dbaa3139ece86f8236c7fc7
cvchukwu <inboxchukwuvictory@gmail.com>
2022-06-22 00:28:36 +0100

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+var firstName = "Victor";
+var lastName = "Chukwu";
+var height = "1.95 metres";
+var country = "Nigeria";
+
+myName = firstName + " " + lastName;
+
+document.write("Name: " + " " + myName +"." + "<br/>");
+document.write("Height: " + " " + height +"." + "<br/>");
+document.write("country: " + " " + country +".");
